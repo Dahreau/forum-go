99d0714ebd12e3aeced1627c03fdaa32bdcbdece
dbourcie <dylan.bourcier.27@gmail.com>
2024-10-21 11:13:43 +0200

    Routes creation

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -4,23 +4,27 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.HelloWorldHandler)
-	mux.HandleFunc("/long-task", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(10 * time.Second) // Simule une tâche longue
-		w.Write([]byte("Task completed"))
-	})
+	mux.HandleFunc("/", s.HomePageHandler)
+	mux.HandleFunc("GET /login", s.GetLoginHandler)
+	mux.HandleFunc("POST /login", s.PostLoginHandler)
+	mux.HandleFunc("POST /logout", s.LogoutHandler)
+	mux.HandleFunc("GET /register", s.GetRegisterHandler)
+	mux.HandleFunc("POST /register", s.PostRegisterHandler)
+	mux.HandleFunc("GET /posts/create", s.GetNewPostsHandler)
+	mux.HandleFunc("POST /posts/create", s.PostNewPostsHandler)
+	mux.HandleFunc("GET /post/{id}", s.GetPostHandler)
+	mux.HandleFunc("POST /post/{id}/comments", s.PostCommentHandler)
 	mux.HandleFunc("/health", s.healthHandler)
 
 	return mux
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HomePageHandler(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
 	resp["message"] = "Hello World"
 
@@ -40,4 +44,23 @@ func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) GetLoginHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) LogoutHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) GetRegisterHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) PostRegisterHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) GetNewPostsHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) PostNewPostsHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) GetPostHandler(w http.ResponseWriter, r *http.Request) {
+}
+func (s *Server) PostCommentHandler(w http.ResponseWriter, r *http.Request) {
 }
