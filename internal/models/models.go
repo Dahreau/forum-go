54447870e80d2d9b0c78663b35b9d1e9a504ca9e
ajoly <alexandre.joly.76300@gmail.com>
2024-10-22 09:23:04 +0200

    Ajout section email

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,13 +3,12 @@ package models
 import "time"
 
 type User struct {
-	UserId       string
-	Username     string
-	Email        string
-	Password     string
-	Role         string
-	CreationDate time.Time
-	SessionId   string
+	UserId         string
+	Username       string
+	Email          string
+	Password       string
+	Role           string
+	CreationDate   time.Time
+	SessionId      string
 	SessionExpired time.Time
 }
-
