dfa47d89c3eb3feffecf0878c192375f585e81cc
student <darosamaky@gmail.com>
2024-10-22 10:44:39 +0200

    added cookie not random

--- internal/server/authenticationHandlers.go
+++ internal/server/authenticationHandlers.go
@@ -11,11 +11,35 @@ import (
 )
 
 func (s *Server) GetLoginHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./assets/login.tmpl.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
+
 }
+
 func (s *Server) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
+	//Simulates login
+	userID := "12345"
+
+	//Creates cookie session
+	expiration := time.Now().Add(24 * time.Hour)
+	cookie := http.Cookie{
+		Name:     "session_id",
+		Value:    userID,
+		Expires:  expiration,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
 func (s *Server) LogoutHandler(w http.ResponseWriter, r *http.Request) {
 }
+
 func (s *Server) GetRegisterHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./assets/register.tmpl.html")
 	if err != nil {
@@ -24,6 +48,7 @@ func (s *Server) GetRegisterHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	t.Execute(w, nil)
 }
+
 func (s *Server) PostRegisterHandler(w http.ResponseWriter, r *http.Request) {
 	user := models.User{Username: r.FormValue("username"), Email: r.FormValue("email"), Password: r.FormValue("password"), Role: "user", CreationDate: time.Now(), UserId: strconv.Itoa(rand.Intn(math.MaxInt32))}
 	err := s.db.CreateUser(user)
