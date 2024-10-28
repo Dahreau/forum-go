5ba57bc72813409568071c546e73bfba6ac2d94a
student <darosamaky@gmail.com>
2024-10-28 18:23:18 +0100

    Added Login Page CSS + Fixed Header + Added header.css + Updated Post Details Page + Other minor fixes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -44,7 +44,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 }
 
 func (s *Server) HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	render(w, r, "../index", nil)
+	render(w, r, "home", nil)
 }
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
