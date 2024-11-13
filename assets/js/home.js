579a2b0b29593fef4f951856ce47c93f6ebc6538
ajoly <alexandre.joly.76300@gmail.com>
2024-11-13 15:48:50 +0100

    Filtering display with js

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,3 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setActiveTab();
 });
+
+const select = document.getElementById("filter-category");
+const selectedCategories = document.getElementById("selected-categories");
+const originalOptions = Array.from(select.options);
+
+function addCategory() {
+  const selectedOption = select.options[select.selectedIndex];
+  if (selectedOption) {
+    const categoryElement = document.createElement("div");
+    categoryElement.className = "category-box";
+    categoryElement.innerHTML = `<span class="remove-btn">×</span> ${selectedOption.text}`;
+    categoryElement.id = `selected-${selectedOption.value}`;
+    categoryElement.onclick = function () {
+      removeCategory(selectedOption.value, selectedOption.text);
+    };
+    selectedCategories.appendChild(categoryElement);
+    select.remove(select.selectedIndex);
+  }
+}
+
+function removeCategory(value, text) {
+  const categoryElement = document.getElementById(`selected-${value}`);
+  if (categoryElement) {
+    categoryElement.remove();
+  }
+  const option = document.createElement("option");
+  option.value = value;
+  option.text = text;
+  select.add(option);
+  sortOptions();
+}
+select.onchange = addCategory;
+
+function sortOptions() {
+  const options = Array.from(select.options).slice(1); // Ignore first option
+  options.sort((a, b) => a.text.localeCompare(b.text));
+  select.innerHTML = '<option value="">Choisissez une catégorie</option>';
+  options.forEach((option) => select.add(option));
+}
